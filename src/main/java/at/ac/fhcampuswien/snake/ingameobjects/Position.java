8a6f120773a47a32c4421974cd289d25aa42af75
FatimaYFK <fykonuk@gmail.com>
2025-01-03 00:42:20 +0100

    moreno/20250106
    Cherry-picked Fadime's changes onto Jessica's addition of sources so we can better see Fadime's changes.

--- src/main/java/at/ac/fhcampuswien/snake/ingameobjects/Position.java
+++ src/main/java/at/ac/fhcampuswien/snake/ingameobjects/Position.java
@@ -1,20 +1,43 @@
 package at.ac.fhcampuswien.snake.ingameobjects;
 
-public class Position {
+import at.ac.fhcampuswien.snake.util.Constants;
 
+public class Position {
     private int x;
     private int y;
 
+    // Konstruktor
     public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
+        this.x = alignToGrid(x);
+        this.y = alignToGrid(y);
     }
 
+    // Getter
     public int getX() {
         return x;
     }
 
     public int getY() {
         return y;
+    }
+
+    // Hilfsmethode zur Rasterausrichtung
+    private int alignToGrid(int coordinate) {
+        return (coordinate / Constants.OBJECT_SIZE_MEDIUM) * Constants.OBJECT_SIZE_MEDIUM;
+    }
+
+    // Überschreibe die equals Methode
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Position)) return false;
+        Position other = (Position) obj;
+        return this.x == other.x && this.y == other.y;
+    }
+
+    // Überschreibe die hashCode Methode
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
     }
 }
