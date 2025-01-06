3540cc7f7f4c8c5cd007416975715a8efb6a145e
Fady German <fady.german@hotmail.com>
2025-01-06 13:34:39 +0100

    Add transitive dependency for javafx.graphics in module-info.java for the type Canvas from module javafx.graphics may not be accessible to clients due to missing 'requires transitive'

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,16 +3,24 @@ module at.ac.fhcampuswien.snake {
     requires javafx.fxml;
     requires org.slf4j;
     requires javafx.media;
-
+    requires transitive javafx.graphics;
 
     opens at.ac.fhcampuswien.snake to javafx.fxml;
+
     exports at.ac.fhcampuswien.snake;
     exports at.ac.fhcampuswien.snake.controller;
+
     opens at.ac.fhcampuswien.snake.controller to javafx.fxml;
+
     exports at.ac.fhcampuswien.snake.util;
+
     opens at.ac.fhcampuswien.snake.util to javafx.fxml;
+
     exports at.ac.fhcampuswien.snake.board;
+
     opens at.ac.fhcampuswien.snake.board to javafx.fxml;
+
     exports at.ac.fhcampuswien.snake.manager;
+
     opens at.ac.fhcampuswien.snake.manager to javafx.fxml;
 }
